8497669c8229225037c94dbe16fa04cec4dc9a3b
Naaman <naamanoussama03@gmail.com>
2024-03-23 09:42:47 +0000

    first commit

---
+++ src/main/java/ma/emsi/patientproject/PatientProjectApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.patientproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientProjectApplication.class, args);
+	}
+
+}
